5e79de773de6f55ac305ac9d0ad77775c456e87f
Deepanshu <deepanshunegi10@gmail.com>
2025-11-24 14:36:39 +0530

    dont know anymore

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Enables static export
-    distDir: 'out',// Saves the static build in the "out" folder
+
     images: {
         unoptimized: true, // 👈 This disables image optimization
     },
